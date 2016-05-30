21960b1f48104782e32e5eac9b6ae68bf6b18c89
azzret <jusuev.azret@bk.ru>
2016-05-31 02:09:43 +0300

    Все сделано урааааа

--- Проект2/Проект2/MyForm.h
+++ Проект2/Проект2/MyForm.h
@@ -34,6 +34,19 @@ namespace Проект2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::ComboBox^  comboBox3;
+	private: System::Windows::Forms::ComboBox^  comboBox4;
+	private: System::Windows::Forms::ComboBox^  comboBox5;
+	private: System::Windows::Forms::Button^  button1;
+	protected: 
 
 	private:
 		/// <summary>
@@ -48,12 +61,175 @@ namespace Проект2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox4 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox5 = (gcnew System::Windows::Forms::ComboBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"House", L"Building ", L"Town", L"Market"});
+			this->comboBox1->Location = System::Drawing::Point(88, 42);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(161, 21);
+			this->comboBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(17, 50);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(30, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Дом";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(17, 90);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(40, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Кошка";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(17, 131);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(48, 13);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Машина";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(17, 172);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(51, 13);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Самолет";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(17, 210);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(33, 13);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"Небо";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(247, 253);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(0, 13);
+			this->label6->TabIndex = 6;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"Dog", L"Cat", L"Bird", L"Lion"});
+			this->comboBox2->Location = System::Drawing::Point(88, 82);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(161, 21);
+			this->comboBox2->TabIndex = 7;
+			// 
+			// comboBox3
+			// 
+			this->comboBox3->FormattingEnabled = true;
+			this->comboBox3->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"Train", L"Car", L"Plane", L"Computer"});
+			this->comboBox3->Location = System::Drawing::Point(88, 123);
+			this->comboBox3->Name = L"comboBox3";
+			this->comboBox3->Size = System::Drawing::Size(161, 21);
+			this->comboBox3->TabIndex = 8;
+			this->comboBox3->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox3_SelectedIndexChanged);
+			// 
+			// comboBox4
+			// 
+			this->comboBox4->FormattingEnabled = true;
+			this->comboBox4->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"Plane", L"Building", L"Super car", L"Bus"});
+			this->comboBox4->Location = System::Drawing::Point(88, 164);
+			this->comboBox4->Name = L"comboBox4";
+			this->comboBox4->Size = System::Drawing::Size(161, 21);
+			this->comboBox4->TabIndex = 9;
+			// 
+			// comboBox5
+			// 
+			this->comboBox5->FormattingEnabled = true;
+			this->comboBox5->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"Duck", L"Animal", L"Mouse", L"Sky"});
+			this->comboBox5->Location = System::Drawing::Point(88, 202);
+			this->comboBox5->Name = L"comboBox5";
+			this->comboBox5->Size = System::Drawing::Size(161, 21);
+			this->comboBox5->TabIndex = 10;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(372, 123);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 27);
+			this->button1->TabIndex = 11;
+			this->button1->Text = L"Проверить";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(459, 328);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->comboBox5);
+			this->Controls->Add(this->comboBox4);
+			this->Controls->Add(this->comboBox3);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void comboBox3_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+			 }
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 int score=0;//баллы по тесту
+			 if(comboBox1->SelectedIndex==0){
+			 score++;
+			 }
+			 if(comboBox2->SelectedIndex==1){
+				 score++;
+			 }
+			 if(comboBox3->SelectedIndex==1){
+				 score++;
+			 }
+			 if(comboBox4->SelectedIndex==0){
+				 score++;
+			 }
+			 if(comboBox5->SelectedIndex==3){
+				 score++;
+			 }
+			 label6->Text="Вы набрали "+score+" баллов";
+			 
+
+		 }
+};
 }
